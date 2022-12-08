83c39dfce90870e31d3c86ce32754ea68f327aa4
@hexlet/cli <support@hexlet.io>
2022-12-08 14:00:12 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 // BEGIN
-class App {
-    public static String[][] enlargeArrayImage(String[][] images) {
-        return Arrays.stream(images)
-                .flatMap(image -> Arrays.stream(image))
-                .map(item -> new String[] {item, item})
-                .toArray(String[][]::new);
-    }
-}
+//class App {
+//    public static String[][] enlargeArrayImage(String[][] images) {
+//        return Arrays.stream(images)
+//                .flatMap(image -> Arrays.stream(image))
+//                .map(item -> new String[] {item, item})
+//                .toArray(String[][]::new);
+//    }
+//}
 // END
