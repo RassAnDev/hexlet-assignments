a193bf56c4b479fffba2eb4e92b34c545ddc2e4c
@hexlet/cli <support@hexlet.io>
2023-07-11 17:35:26 +0300

    submit java-web-ru/jdbc

--- java-web-ru/jdbc/src/main/java/exercise/servlet/ArticlesServlet.java
+++ java-web-ru/jdbc/src/main/java/exercise/servlet/ArticlesServlet.java
@@ -67,6 +67,38 @@ public class ArticlesServlet extends HttpServlet {
         ServletContext context = request.getServletContext();
         Connection connection = (Connection) context.getAttribute("dbConnection");
         // BEGIN
+//        List<Map<String, String>> articles = new ArrayList<>();
+//
+//        int articlesPerPage = 10;
+//
+//        String page = request.getParameter("page");
+//        int normalizedPage = page == null ? 1 : Integer.parseInt(page);
+//        int offset = (normalizedPage - 1) * articlesPerPage;
+//
+//        String query = "SELECT id, title FROM articles ORDER BY id LIMIT ? OFFSET ?";
+//
+//        try {
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            statement.setInt(1, articlesPerPage);
+//            statement.setInt(2, offset);
+//            ResultSet rs = statement.executeQuery();
+//
+//            while (rs.next()) {
+//                articles.add(Map.of(
+//                                "id", rs.getString("id"),
+//                                "title", rs.getString("title")
+//                        )
+//                );
+//            }
+//
+//        } catch (SQLException e) {
+//            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//            return;
+//        }
+//
+//        request.setAttribute("articles", articles);
+//        request.setAttribute("page", normalizedPage);
+
         List<Map<String, String>> articles = new ArrayList<>();
         String page = request.getParameter("page");
 
@@ -103,6 +135,33 @@ public class ArticlesServlet extends HttpServlet {
         ServletContext context = request.getServletContext();
         Connection connection = (Connection) context.getAttribute("dbConnection");
         // BEGIN
+//        String id = getId(request);
+//
+//        Map<String, String> article = new HashMap<>();
+//
+//        String query = "SELECT * FROM articles WHERE id=?";
+//
+//        try {
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            statement.setString(1, id);
+//            ResultSet rs = statement.executeQuery();
+//
+//            if (!rs.first()) {
+//                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+//                return;
+//            }
+//
+//            article.put("title", rs.getString("title"));
+//            article.put("body", rs.getString("body"));
+//
+//
+//        } catch (SQLException e) {
+//            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//            return;
+//        }
+//
+//        request.setAttribute("article", article);
+
         String id = getId(request);
 
         Map<String, String> article = new HashMap<>();
